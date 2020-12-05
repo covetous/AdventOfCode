8604f95d097a40fd1706fd7d7748acde404a4640
Niklas Asp <niklas@izedlab.se>
2020-12-05 07:32:59 +0100

    Day 4, part 1 löst. Day 5 setup

--- DayFour.py
+++ DayFour.py
@@ -1,3 +1,15 @@
+MANDATORY_FIELDS = [
+    'byr',
+    'iyr',
+    'eyr',
+    'hgt',
+    'hcl',
+    'ecl',
+    'pid',
+    # 'cid'
+]
+
+
 def read_file(expense_file):
     file_object = open(expense_file, 'r')
     return file_object.readlines()
@@ -5,18 +17,53 @@ def read_file(expense_file):
 
 def split_into_passports(pass_file):
     passports = []
-    i=0
-    while i < len(pass_file):
-        if len(pass_file[i]) == 0:
+    passport = {}
+    blank_lines = 0
+    for line in pass_file:
+        print(f'Line from file: {line}')
+        if not line.strip():
+            print(f'New passport: {passport}')
             passports.append(passport)
+            passport = {}
+            blank_lines += 1
+        else:
+            line.strip()
+            fields = line.split(' ')
+            for field in fields:
+                values = field.split(':')
+                passport.update({values[0].strip(): values[1].strip()})
 
-        i += 1
+    print(f'New passport: {passport}')
+    passports.append(passport)
+    blank_lines += 1
+
+    print(f'Found {len(passports)} passports')
+    print(f'Found {blank_lines} blank lines')
+    return passports
 
 
-def main(file = "passport_database.txt"):
+def verify_passports(passports):
+    is_valid = 0
+    not_valid = 0
+    for passport in passports:
+        is_ok = True
+        for field in MANDATORY_FIELDS:
+            if passport.get(field) is None:
+                print(f'Passport NOT valid {passport}')
+                not_valid += 1
+                is_ok = False
+        if is_ok:
+            print(f'Passport valid {passport}')
+            is_valid += 1
+    print(f'{not_valid} Not valid passports ')
+    return is_valid
+
+
+def main(file="passport_database.txt"):
     pass_file = read_file(file)
-    passports = []
     passports = split_into_passports(pass_file)
+    result = verify_passports(passports)
+    print(f'Correct passports: {result}')
 
 
 if __name__ == '__main__':
